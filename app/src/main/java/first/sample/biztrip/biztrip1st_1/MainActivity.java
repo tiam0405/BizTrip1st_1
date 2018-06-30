ed3ff388d0d7014e4eb0658aba9c9ed50bea81fb
혜박 <chr0918@gmail.com>
2018-06-30 17:35:49 +0900

    allcommit

---
+++ app/src/main/java/first/sample/biztrip/biztrip1st_1/MainActivity.java
@@ -0,0 +1,20 @@
+package first.sample.biztrip.biztrip1st_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View v) {
+        Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show();
+    }
+
+}
